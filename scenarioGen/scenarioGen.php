56826013a3513bc74e028b6f4c6770cab6f48b3c
dabame <dbm0100@yahoo.com>
2015-11-13 12:48:10 -0500

    fully functional scenario generator

--- scenarioGen/scenarioGen.php
+++ scenarioGen/scenarioGen.php
@@ -1,29 +1,30 @@
 <?php
-
 /*****************************************************
  * Scenario Generator
  * Author: dabame dbm0100@yahoo.com
  * Generates scenarios for play in dabame_game
  ****************************************************/
-
 class Scenario{
     function __construct(){
         $this->setting = $this->randomLine("scenarioGen/settings.txt");
-    }
-    
-    private $_antagonists; // array of antagonists in scenario
-    private $_complications; // array of complications in scenario
-    private $_objectives; // array of objectives in scenario
+		$this->objective = $this->randomLine("scenarioGen/objectives.txt");
+		$this->antagonist = $this->randomLine("scenarioGen/antagonists.txt");
+		$this->complications = $this->randomLine("scenarioGen/complications.txt");
+	}
+    private $antagonist; // antagonists in scenario
+    private $complications; // complications in scenario
+    private $objective; // objective in scenario
     private $setting; // the setting for the scenario
 
     public function displayScenario(){
-        echo $this->setting;
+        echo "$this->setting  $this->complications  $this->objective  $this->antagonist";
     }
     
     // fetches a random line from a file
     private function randomLine($filename){
         $lines = file($filename);
-        return $lines[array_rand($lines)];
+		$randLine = preg_replace("/[\n\r]/"," ",$lines[array_rand($lines)]);
+        return $randLine;
     }
 }
 
@@ -31,3 +32,4 @@ class Scenario{
 $myScenario = new Scenario();
 $myScenario->displayScenario();
 ?>
+
